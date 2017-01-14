8dbb8cb94b79260035812fea24c00bbe146ebc4c
Amanda Morris <amandafmorris@gmail.com>
2017-01-14 23:15:47 +0000

    Start of parts 2-3

--- assessment.py
+++ assessment.py
@@ -56,3 +56,41 @@ Part 1: Discussion
 
 # Parts 2 through 5:
 # Create your classes and class methods
+
+class Student(object):
+
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+
+class Question(object):
+
+    def __init__(self, question, answer):
+        self.question = question
+        self.correct_answer = answer
+
+    def ask_and_evaluate(self):
+        user_answer = raw_input(self.question + " > ")
+        if user_answer.lower() == self.correct_answer:
+            print True
+        else:
+            print False
+
+
+class Exam(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.questions = []
+
+    def add_question(self, question, correct_answer):
+        self.questions.append({"question": question,
+                               "correct_answer": correct_answer
+                               })
+
+    def administer(self):
+        score = 0
+        for question in self.questions:
+            
